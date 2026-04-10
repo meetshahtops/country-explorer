956bb18e60f66bb09ce69823dbace9b64cca9969
meetshahtops <meet@topsinfosolutions.com>
2026-04-10 17:13:58 +0530

    icons working now

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,12 @@ export default function RootLayout({
       lang="en"
       className={`${manrope.variable} ${inter.variable} dark`}
     >
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
+        />
+      </head>
       <body className="min-h-screen flex flex-col font-body antialiased">
         {children}
       </body>
